9f98aed9390fa15e0083fcdaea124071d7e4e0f9
BautistaFernandez <141528279+BautistaFernandez@users.noreply.github.com>
2024-08-09 16:39:39 -0300

    Add files via upload

---
+++ myInfo_project/pagescripts.js
@@ -0,0 +1,4 @@
+document.getElementById("watch-this").addEventListener("click", function() {
+    alert("¿Qué es un algoritmo? Una palabra que usan los programadores cuando no quieren explicar lo que hicieron.");
+});
+
